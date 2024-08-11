1aa2646cb8cf1b8bfc2d8f926a6e5d9993014f52
Mikonimo <mikonani413@gmail.com>
2024-08-11 23:05:17 +0300

    api

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-    
+
     def get(self, cls, id):
         """Retrieve one object based on the class and its ID."""
         objects = self.all(cls)
