e2b4c81ab39c007bf525c8218e3b2023f96917c8
Mikonimo <mikonani413@gmail.com>
2024-08-11 23:25:05 +0300

    pycode

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,7 @@
 """Blueprint setup"""
 
 from flask import Blueprint
+from api.v1.views.index import *
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import routes so they are registered with the Blueprint
-from api.v1.views.index import *
